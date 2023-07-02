67aec0d8de6465bea6dfe2f83f11850ea4e37886
Abiodun bajulaiye <42373503+Abzion@users.noreply.github.com>
2023-07-02 03:15:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+
 $(document).ready(function() {
-    $('.nav-button').click(function(){
-        $('.nav-button').toggleClass('change');
-    });
+    // $('.nav-button').click(function(){
+    //     $('.nav-button').toggleClass('change');
+    // });
 
 
     
@@ -9,15 +10,40 @@ $(document).ready(function() {
      $(Window).scroll(function(){
         let position = $(this).scrollTop();
         console.log(position);
-        if(position >= 1200) {
+        if(position >= 400) {
             $('.baner-headin').addClass('banner-heading');
-            $('.banner-hading0').addClass('banner-heading0');
-            $('.banner-heding1').addClass('banner-heading1');
+            
 
         }
         else {
             $('.baner-headin').removeClass('banner-heading');
+            
+        }
+     });
+
+     $(Window).scroll(function(){
+        let position = $(this).scrollTop();
+        console.log(position);
+        if(position >= 400) {
+            $('.banner-hading0').addClass('banner-heading0');
+
+        }
+        else {
             $('.banner-hading0').removeClass('banner-heading0');
+
+        }
+     });
+
+     $(Window).scroll(function(){
+        let position = $(this).scrollTop();
+        console.log(position);
+        if(position >= 400) {
+           
+            $('.banner-heding1').addClass('banner-heading1');
+
+        }
+        else {
+
             $('.banner-heding1').removeClass('banner-heading1');
 
         }
@@ -28,39 +54,119 @@ $(document).ready(function() {
     $(Window).scroll(function(){
         let position = $(this).scrollTop();
         console.log(position);
-        if(position >= 1800) {
+        if(position >= 1000) {
             $('.anner-heading1').addClass('banner-heading1');
-            $('.r-heading1').addClass('banner-heading1');
-            $('.r-heading1').addClass('banner-heading1');
-            $('.er-heading0').addClass('banner-heading0');
+            
 
 
         }
         else {
             $('.anner-heading1').removeClass('banner-heading1');
+            
+        }
+     });
+
+     $(Window).scroll(function(){
+        let position = $(this).scrollTop();
+        console.log(position);
+        if(position >= 1000) {
+            $('.r-heading1').addClass('banner-heading1');
+           
+
+        }
+        else {
             $('.r-heading1').removeClass('banner-heading1');
+           
+        }
+     });
+
+     $(Window).scroll(function(){
+        let position = $(this).scrollTop();
+        console.log(position);
+        if(position >= 1000) {
+            
+            $('.r-heading1').addClass('banner-heading1');
+
+
+        }
+        else {
+            
             $('.r-heading1').removeClass('banner-heading1');
+
+        }
+     });
+
+     $(Window).scroll(function(){
+        let position = $(this).scrollTop();
+        console.log(position);
+        if(position >= 1000) {
+           
+            $('.er-heading0').addClass('banner-heading0');
+
+
+        }
+        else {
+           
             $('.er-heading0').removeClass('banner-heading0');
 
         }
      });
 
+    //  second phase
+     $(Window).scroll(function(){
+        let position = $(this).scrollTop();
+        console.log(position);
+        if(position >= 1300) {
+            $('.er-par2').addClass('banner-par2');
+           
+
+        }
+        else {
+            $('.er-par2').removeClass('banner-par2');
+           
+        }
+     });
 
      $(Window).scroll(function(){
         let position = $(this).scrollTop();
         console.log(position);
-        if(position >= 2100) {
-            $('.er-par2').addClass('banner-par2');
+        if(position >= 1300) {
             $('.a-heading1').addClass('banner-heading1');
-            $('.a-heading1').addClass('banner-heading1');
+            
+        }
+        else {
+            $('.a-heading1').removeClass('banner-heading1');
+            
+        }
+     });
+
+     $(Window).scroll(function(){
+        let position = $(this).scrollTop();
+        console.log(position);
+        if(position >= 1300) {
+           
             $('.a-heading1').addClass('banner-heading1');
 
 
         }
         else {
-            $('.er-par2').removeClass('banner-par2');
+           
             $('.a-heading1').removeClass('banner-heading1');
-            $('.a-heading1').removeClass('banner-heading1');
+
+        }
+     });
+
+     $(Window).scroll(function(){
+        let position = $(this).scrollTop();
+        console.log(position);
+        if(position >= 1300) {
+            
+            $('.a-heading1').addClass('banner-heading1');
+
+
+        }
+        else {
+            
             $('.a-heading1').removeClass('banner-heading1');
 
         }
